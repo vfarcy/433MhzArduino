3f9f2fa82a501bb83941e22a310448ae4f8992b0
vfarcy <vfarcy@free.fr>
2014-06-08 09:19:38 +0200

    Increased serial speed from 9600 to 115200bds

--- Receive433Mhz/Receive433Mhz.ino
+++ Receive433Mhz/Receive433Mhz.ino
@@ -12,7 +12,7 @@ RCSwitch mySwitch = RCSwitch();
 int compteur = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   mySwitch.enableReceive(0);  // Receiver on inerrupt 0 => that is pin #2
 }
 
